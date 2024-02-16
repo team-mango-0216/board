857559e065a238cf500434bcdb478afafb054f50
DESKTOP-IFMN175\804-03 <mangoperry2015@gmail.com>
2024-02-16 20:57:36 +0900

    [feat] register
    *기능 요약
    - 게시물(title, content) 업로드
    
    *세부사항
    JPA로 DB 만들기
    Service 로직 개발(get, post) : DB 저장
    Controller 개발
    Swagger 테스트

--- src/main/java/org/teammango/controller/BoardController.java
+++ src/main/java/org/teammango/controller/BoardController.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.teammango.dto.BoardDTO;
 import org.teammango.service.BoardService;
 
-@Controller
+@RestController
 @Log4j2
 @RequestMapping("/board")
 @RequiredArgsConstructor
