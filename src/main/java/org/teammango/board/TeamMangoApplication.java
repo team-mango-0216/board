4388617545cfcddd37b243687848cf65c4c11fba
DESKTOP-IFMN175\804-03 <mangoperry2015@gmail.com>
2024-02-16 12:58:15 +0900

    [TeamMango] first commit

---
+++ src/main/java/org/teammango/board/TeamMangoApplication.java
@@ -0,0 +1,13 @@
+package org.teammango.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMangoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/teammango/board/TeamMangoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teammango.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
